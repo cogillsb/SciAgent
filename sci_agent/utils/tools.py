586b6416a72b74b7e06d47896df51e2e3df100ee
cogillsb <cogillsb@Stanford.edu>
2025-02-06 06:31:37 -0500

    Added decorators to tool methods

--- sci_agent/utils/tools.py
+++ sci_agent/utils/tools.py
@@ -2,15 +2,17 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 
 tools = [TavilySearchResults(max_results=1)]
 
-@tools
+@tool
 def write_to_db(dat):
     """This is a function to output data to the datbase"""
     print('hw')
 
+@tool
 def ask_user_for_a_file():
     """This is a function to let the agent ask the user for a file"""
     return
 
+@tool
 def assign_unique_identifier():
     """This is a function to let the agent look at the files in storage,
     identify an available ID number,
