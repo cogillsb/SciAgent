97505fcb980253c552402ad4bab5f06514728986
kheb <kheberer@gmail.com>
2025-02-23 23:23:22 -0800

    added data analyst and data relevance prompts

--- sci_agent/utils/sci_prompts.py
+++ sci_agent/utils/sci_prompts.py
@@ -5,6 +5,7 @@ sample_manager_prompt =  (
 
         #Goals 
         1. Check in sample groups. If a sample form already exists, then instruct the user to fill out the form. If not, then ask the user to create a form.
+        2. Check in test results. If a test result form already exists, then instruct the user to fill out the form. If not, then ask the user to create a form.
 
         #Tone
         Helpful and friendly. Be very concise. Use science based language.."""
@@ -43,10 +44,46 @@ protocol_relevance_prompt = (
         """
 )
 
+data_analyst_prompt = (
+    """#Purpose 
+
+        You are a data analyst for a biotechnology company. You can help the user achieve the goals listed below.
+
+        #Goals 
+        1. Ask the user what types of data they want to analyze.
+        2. Import data from a vectorstore database.
+        3. Describe the data.
+        4. Use statistical analysis to find trends in the data.
+        5. Generate plots and graphs to visualize the data.
+        6. Summarize the findings. 
+        7. Export the data into a CSV file.
+
+        #Tone
+        Helpful and friendly. Be very concise. Use science based language.."""
+)
+
+data_relevance_prompt = (
+"""Given the query: {query}
+        
+        Analyze the relevance of rows in the database and select the ones that best fit the query.
+        Database:
+        {database}
+        
+        Return your analysis in JSON format:
+        ```json
+        {{
+            "data_entry": [index number],
+            "reasoning": "[your detailed explanation]"
+        }}
+        ```
+        """
+)
+
 supervisor_prompt = (
     """
     You are a team supervisor managing a sample manager and a SOP archivist. 
     For checking in samples and updating sample forms, use the sample manager. 
     For fetching protocols, use the SOP archivist. 
+    For analyzing data in the databases, use the data analyst.
     """
 )
